a2d7083008ff43888d79c234e7ab142637ce897c
Chung Yu <chungyu710@gmail.com>
2021-07-25 21:29:29 -0400

    added wiring info

--- transmitter_module_arduino/transmitter_module_arduino.ino
+++ transmitter_module_arduino/transmitter_module_arduino.ino
@@ -1,11 +1,20 @@
 /*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
+* Sample code by Dejan Nedelkovski, www.HowToMechatronics.com
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
+
+/*
+ * ---------- WIRING ----------
+ * ARDUINO UNO  -> nRF24L01 module
+ * Vcc          -> 3V3
+ * GND          -> GND
+ * IO7          -> CE   (chip enable)
+ * IO8          -> CSN  (SPI chip select) (active low?)
+ * IO11         -> MOSI (SPI master out, slave in)
+ * IO12         -> MISO (SPI master in, slave out)
+ * IO13         -> SCK  (SPI clock)
+ */
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
